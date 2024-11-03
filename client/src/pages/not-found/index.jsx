90461da7a931990996fbd2889d962db3afdc60b7
Abhishek Surendran <surendranabhishek91@gmail.com>
2024-11-03 10:45:54 +0530

    Client base url edit

--- client/src/pages/not-found/index.jsx
+++ client/src/pages/not-found/index.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <div>page doesn't exists</div>;
+  return (
+    <>
+    <div className="text-center"></div>
+    <h1 className="text-3xl font-bold">Page doesn't exists</h1>
+    <Link to="/">Go Home</Link>
+    </>
+  )
 }
 
 export default NotFound;
--- client/src/pages/unauth-page/index.jsx
+++ client/src/pages/unauth-page/index.jsx
@@ -1,5 +1,5 @@
 function UnauthPage() {
-  return <h1>You don't have access to view this page</h1>;
+  return <h1 className="text-center text-3xl font-bold">You don't have access to view this page</h1>;
 }
 
 export default UnauthPage;
